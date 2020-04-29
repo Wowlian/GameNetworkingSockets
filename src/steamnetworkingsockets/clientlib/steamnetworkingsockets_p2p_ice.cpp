Wowlian/GameNetworkingSockets

commit 57271d6c9e590aedd0b7c1e4e828ca355da4b949

Fix deadlock
Happens if we delete ICE session from within a callback.  We can wait to
delete the session, this code was just being a bit over-enthusiastic

src/steamnetworkingsockets/clientlib/steamnetworkingsockets_p2p_ice.cpp
@@ -229,13 +229,8 @@ void CConnectionTransportP2PICE::NotifyConnectionFailed( int nReasonCode, const 
 
 void CConnectionTransportP2PICE::QueueSelfDestruct()
 {
-
-	// Go ahead and free up our ICE session now, it is reference counted.
-	if ( m_pICESession )
-	{
-		m_pICESession->Destroy();
-		m_pICESession = nullptr;
-	}
+	// NOTE: Do *not* attempt to delete the ICE session here.  We don't have enough
+	//       context if that is safe to do.
 
 	// Queue us for deletion
 	if ( Connection().m_pTransportICEPendingDelete )

commit 80230120f1761fd87499ebcf4493a4e0d29cc4c2

Fix CConnectionTransportP2PICE::Think
We might still have an ICE session, even when we are pending to be destroyed.
This was broken by 57271d6c9e590aedd0b7c1e4e828ca355da4b949

src/steamnetworkingsockets/clientlib/steamnetworkingsockets_p2p_ice.cpp
@@ -267,7 +267,7 @@ void CConnectionTransportP2PICE::ScheduleSendSignal( const char *pszReason )
 void CConnectionTransportP2PICE::Think( SteamNetworkingMicroseconds usecNow )
 {
 	// Are we dead?
-	if ( !m_pICESession )
+	if ( !m_pICESession || Connection().m_pTransportICEPendingDelete )
 	{
 		Connection().CheckCleanupICE();
 		// We could be deleted here!