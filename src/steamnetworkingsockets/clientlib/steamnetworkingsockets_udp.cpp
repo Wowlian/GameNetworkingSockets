Wowlian/GameNetworkingSockets

commit c66ba0efbfb1bb4845d6e0bc6f085e6d79a167a5

Delete CSteamNetworkConnectionBase::m_netAdrRemote
As it turns out, we never actually needed it.  And as several FIXMEs noted,
this was the wrong place for it to be.

src/steamnetworkingsockets/clientlib/steamnetworkingsockets_udp.cpp
@@ -418,13 +418,12 @@ void CSteamNetworkListenSocketDirectUDP::Received_ConnectRequest( const CMsgStea
 	{
 		CSteamNetworkConnectionBase *pOldConn = m_mapChildConnections[ h ];
 		Assert( pOldConn->m_identityRemote == identityRemote );
-		Assert( pOldConn->GetRemoteAddr() != adrFrom ); // or else why didn't we already map it directly to them!
 
 		// NOTE: We cannot just destroy the object.  The API semantics
 		// are that all connections, once accepted and made visible
 		// to the API, must be closed by the application.
-		ReportBadPacket( "ConnectRequest", "Rejecting connection request from %s at %s, connection ID %u.  That steamID/ConnectionID pair already has a connection from %s\n",
-			SteamNetworkingIdentityRender( identityRemote ).c_str(), CUtlNetAdrRender( adrFrom ).String(), unClientConnectionID, CUtlNetAdrRender( pOldConn->GetRemoteAddr() ).String()
+		ReportBadPacket( "ConnectRequest", "Rejecting connection request from %s at %s, connection ID %u.  That steamID/ConnectionID pair already has a connection [%s]\n",
+			SteamNetworkingIdentityRender( identityRemote ).c_str(), CUtlNetAdrRender( adrFrom ).String(), unClientConnectionID, pOldConn->GetDescription()
 		);
 
 		CMsgSteamSockets_UDP_ConnectionClosed msgReply;
@@ -815,9 +814,6 @@ bool CSteamNetworkConnectionUDP::BInitConnect( const SteamNetworkingIPAddr &addr
 	Assert( m_identityRemote.IsInvalid() );
 	m_identityRemote.Clear();
 
-	// We know what the remote addr will be.
-	m_netAdrRemote = netadrRemote;
-
 	// We should know our own identity, unless the app has said it's OK to go without this.
 	if ( m_identityLocal.IsInvalid() ) // Specific identity hasn't already been set (by derived class, etc)
 	{
@@ -959,7 +955,6 @@ bool CSteamNetworkConnectionUDP::BBeginAccept(
 	Assert( !m_identityRemote.IsInvalid() );
 
 	m_unConnectionIDRemote = unConnectionIDRemote;
-	m_netAdrRemote = adrFrom;
 	if ( !pParent->BAddChildConnection( this, errMsg ) )
 		return false;
 

src/steamnetworkingsockets/clientlib/steamnetworkingsockets_connections.cpp
@@ -1642,9 +1642,6 @@ void CSteamNetworkConnectionBase::ConnectionPopulateInfo( SteamNetConnectionInfo
 
 	info.m_eState = CollapseConnectionStateToAPIState( m_eConnectionState );
 	info.m_hListenSocket = m_pParentListenSocket ? m_pParentListenSocket->m_hListenSocketSelf : k_HSteamListenSocket_Invalid;
-	NetAdrToSteamNetworkingIPAddr( info.m_addrRemote, m_netAdrRemote ); // FIXME this is in a weird place
-	info.m_idPOPRemote = 0;
-	info.m_idPOPRelay = 0;
 	info.m_identityRemote = m_identityRemote;
 	info.m_nUserData = m_nUserData;
 	info.m_eEndReason = m_eEndReason;

src/steamnetworkingsockets/clientlib/steamnetworkingsockets_connections.h
@@ -11,7 +11,6 @@
 #endif
 #include "../steamnetworking_statsutils.h"
 #include <tier1/utlhashmap.h>
-#include <tier1/netadr.h>
 #include "steamnetworkingsockets_lowlevel.h"
 #include "../steamnetworkingsockets_thinker.h"
 #include "keypair.h"
@@ -291,11 +290,6 @@ public:
 	/// (The wire protocol doesn't care about local states such as linger)
 	bool BStateIsConnectedForWirePurposes() const { return m_eConnectionState == k_ESteamNetworkingConnectionState_Connected || m_eConnectionState == k_ESteamNetworkingConnectionState_Linger; }
 
-	/// Accessor for remote address (if we know it)
-	/// FIXME - Should we delete this and move to derived classes?
-	/// It's not always meaningful
-	const netadr_t &GetRemoteAddr() const { return m_netAdrRemote; }
-
 	/// Reason connection ended
 	ESteamNetConnectionEnd GetConnectionEndReason() const { return m_eEndReason; }
 	const char *GetConnectionEndDebugString() const { return m_szEndDebug; }
@@ -517,11 +511,6 @@ protected:
 
 	/// Called from BInitConnection, to start obtaining certs, etc
 	virtual void InitConnectionCrypto( SteamNetworkingMicroseconds usecNow );
-
-	/// If this is a direct UDP connection, what is the address of the remote host?
-	/// FIXME - Should we delete this and move to derived classes?
-	/// It's not always meaningful
-	netadr_t m_netAdrRemote;
 
 	/// User data
 	int64 m_nUserData;

commit 1db2c8a01da859709a916ed8489cd7679fcafe09

Fix bug not populating peer remote address
This was broken in c66ba0efbfb1bb4845d6e0bc6f085e6d79a167a5.

src/steamnetworkingsockets/clientlib/steamnetworkingsockets_udp.cpp
@@ -1092,6 +1092,7 @@ void CConnectionTransportUDP::TransportPopulateConnectionInfo( SteamNetConnectio
 	if ( m_pSocket )
 	{
 		const netadr_t &addr = m_pSocket->GetRemoteHostAddr();
+		NetAdrToSteamNetworkingIPAddr( info.m_addrRemote, addr );
 		if ( addr.IsLoopback() )
 			info.m_eTransportKind = k_ESteamNetTransport_LocalHost;
 		else if ( addr.IsReservedAdr() )